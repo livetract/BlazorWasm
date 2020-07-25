cc0bf9f91274ce4a5e81df3a5f7cd18899c96c75
YD <abc_hyd@outlook.com>
2020-07-25 19:53:18 +0800

    创建todo数据api

---
+++ WeatherApi/Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WeatherApi.Models;
+
+namespace WeatherApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        public TodoController()
+        {
+            TodoItems = new List<Todo> {
+                new Todo{Id = Guid.NewGuid(), Title = "创建Todo Item", Content = "创建 Todo Item 用来进行工作和任务的督导", IsCompleted = false},
+                new Todo{Id = Guid.NewGuid(), Title = "吃晚饭", Content = "下班了，要按时吃晚饭。", IsCompleted = true}
+                };
+        }
+
+        private readonly List<Todo> TodoItems;
+
+        public ActionResult<IEnumerable<Todo>> GetItems()
+        {
+
+
+            return Ok(TodoItems);
+        }
+    }
+}
---
+++ WeatherApi/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WeatherApi.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
