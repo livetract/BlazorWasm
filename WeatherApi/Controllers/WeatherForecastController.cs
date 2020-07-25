69de293b90af0e2b0c7aa0d11c436fd0040b2136
YD <abc_hyd@outlook.com>
2020-07-25 19:23:30 +0800

    weatherforecast更改为公共数据

--- WeatherApi/Controllers/WeatherForecastController.cs
+++ WeatherApi/Controllers/WeatherForecastController.cs
@@ -1,16 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using WeatherApi.Models;
 
 namespace WeatherApi.Controllers
 {
     [ApiController]
-    [Authorize]
     [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
