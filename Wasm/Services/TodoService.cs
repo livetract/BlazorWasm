6088df2a15e1df2e81476edb1823ac4ab8e50e32
YD <abc_hyd@outlook.com>
2020-07-28 21:09:42 +0800

    删除重复语句

--- Wasm/Services/TodoService.cs
+++ Wasm/Services/TodoService.cs
@@ -1,8 +1,5 @@
-﻿using Blazored.LocalStorage;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Wasm.Dtos;
@@ -12,24 +9,15 @@ namespace Wasm.Services
     public class TodoService : ITodoService
     {
         private readonly HttpClient _httpClient;
-        private readonly ILocalStorageService localStorage;
         private const string BaseUri = "https://localhost:9011/api/todo";
 
-        public TodoService(HttpClient httpClient,ILocalStorageService localStorage)
+        public TodoService(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            this.localStorage = localStorage;
         }
         
         public async Task<IEnumerable<TodoItem>> GetItemsAsync()
         {
-
-            var token = await localStorage.GetItemAsStringAsync("authToken");
-            if (string.IsNullOrEmpty(token))
-            {
-                return await _httpClient.GetFromJsonAsync<IEnumerable<TodoItem>>(BaseUri);
-            }
-            
             return await _httpClient.GetFromJsonAsync<IEnumerable<TodoItem>>(BaseUri);
         }
     }
